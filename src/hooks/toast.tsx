0e19471e8581aa5b2d24bb429415546ddecff92e
Michel Popolin de Freitas <micpopolin@gmail.com>
2020-11-10 20:26:43 -0300

    toasts

---
+++ src/hooks/toast.tsx
@@ -0,0 +1,60 @@
+import React, { createContext, useContext, useCallback, useState } from 'react';
+// import { uuid } from 'uuidv4';
+import { v4 } from 'uuid';
+
+import ToastContainer from '../components/ToastContainer';
+
+export interface ToastMessage {
+  id: string;
+  type?: 'success' | 'error' | 'info';
+  title: string;
+  description?: string;
+}
+
+interface ToastContextData {
+  addToast(message: Omit<ToastMessage, 'id'>): void;
+  removeToast(id: string): void;
+}
+
+const toastContext = createContext({} as ToastContextData);
+
+const ToastProvider: React.FC = ({ children }) => {
+  const [messages, setMessages] = useState<ToastMessage[]>([]);
+
+  const addToast = useCallback(
+    ({ type, title, description }: Omit<ToastMessage, 'id'>) => {
+      const id = v4();
+
+      const toast = {
+        id,
+        type,
+        title,
+        description,
+      };
+
+      setMessages(oldMessages => [...oldMessages, toast]);
+    },
+    [],
+  );
+
+  const removeToast = useCallback((id: string) => {
+    setMessages(state => state.filter(message => message.id !== id));
+  }, []);
+
+  return (
+    <toastContext.Provider value={{ addToast, removeToast }}>
+      {children}
+      <ToastContainer messages={messages} />
+    </toastContext.Provider>
+  );
+};
+
+function useToast(): ToastContextData {
+  const context = useContext(toastContext);
+  if (!context) {
+    throw new Error('useToast must be used within a ToastProvider');
+  }
+  return context;
+}
+
+export { ToastProvider, useToast };
